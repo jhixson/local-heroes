efe4f104d3803ef1f68fc5af2f6c066c8ee84c57
Jesse Hixson <hixsonj@gmail.com>
2011-10-31 11:31:03 -0400

    Created fresh rails app (3.1.1)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LocalHeroes::Application.config.secret_token = '4794e37c0861f45439a42c97c70866f92449d38355271b75a66a6c854b403b6a0299e50176c595fd7b1b5094c7377a2d7ffc6063236483ad79f7b89ebb470663'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+LocalHeroes::Application.config.session_store :cookie_store, key: '_local-heroes_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# LocalHeroes::Application.config.session_store :active_record_store
