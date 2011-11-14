4cfa8303e3fb53047bc4ecfc6981ecf950ecdb35
Jesse Hixson <hixsonj@gmail.com>
2011-11-14 13:55:49 -0500

    Added locations scaffold and some routes.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20111113165711) do
+
+  create_table "locations", :force => true do |t|
+    t.string   "city"
+    t.string   "state"
+    t.string   "zip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
